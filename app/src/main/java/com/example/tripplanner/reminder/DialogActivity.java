0b4d6b0efb16a834051f3e02f786aa4ab1457901
Manar <manaraelkady@gmail.com>
2020-02-21 20:06:13 +0200

    add DialogActivity Visibility

--- app/src/main/java/com/example/tripplanner/reminder/DialogActivity.java
+++ app/src/main/java/com/example/tripplanner/reminder/DialogActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Window;
 
 public class DialogActivity extends AppCompatActivity {
     Context context;
     AlertDialog mAlertDialog = null;
     AlertDialog.Builder mAlertDialogBuilder = null;
-    TimeUpAlertDialog timeUpAlertDialog;
+    TimeUpAlertDialog timeUpAlertDialog = new TimeUpAlertDialog();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.context = this;
+        this.requestWindowFeature (Window.FEATURE_NO_TITLE);
+        getWindow ().setBackgroundDrawableResource (android.R.color.transparent);
         timeUpAlertDialog.showAlertDialog(context, "Time is Up, Would you like to start your trip?",
                 "click START to navigate to your trip");
     }
