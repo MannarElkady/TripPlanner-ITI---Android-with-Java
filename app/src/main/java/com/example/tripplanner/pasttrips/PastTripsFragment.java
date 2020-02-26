4758e2d0e921d204ddcae58ce80397335b290722
RehamEzzat <rehamezzatmohamed@gmail.com>
2020-02-27 00:40:17 +0200

    past trips

--- app/src/main/java/com/example/tripplanner/pasttrips/PastTripsFragment.java
+++ app/src/main/java/com/example/tripplanner/pasttrips/PastTripsFragment.java
@@ -3,11 +3,16 @@ package com.example.tripplanner.pasttrips;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.tripplanner.R;
 
@@ -15,6 +20,11 @@ import com.example.tripplanner.R;
  * A simple {@link Fragment} subclass.
  */
 public class PastTripsFragment extends Fragment {
+    private PastTripsViewModel pastTripsViewModel;
+    private PastTripsAdapter pastTripsAdapter;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView recyclerView;
+    private LinearLayout noPastTripsLayout;
 
 
     public PastTripsFragment() {
@@ -25,8 +35,24 @@ public class PastTripsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_past_trips, container, false);
+        View view = inflater.inflate(R.layout.fragment_past_trips, container, false);
+
+        noPastTripsLayout = view.findViewById(R.id.no_past_trips_layout);
+        recyclerView = view.findViewById(R.id.past_trips_recyclerview);
+
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        pastTripsViewModel = new ViewModelProvider(requireActivity()).get(PastTripsViewModel.class);
+
+        recyclerView.setHasFixedSize(true);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+    }
 }
